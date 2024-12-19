6d6c3f5512da92baf82b29fca6ec30387e426ef8
Pavel Pozdnyakov <pavel.pozdnyakov@bedag.ch>
2024-12-19 15:48:32 +0100

    add solution for day 19 and stub for day 20

--- 2024/day-19/main-b.py
+++ 2024/day-19/main-b.py
@@ -1,14 +1,13 @@
-def is_possible(design: str, patterns: list[str]) -> bool:
+def is_possible(design: str, patterns: list[str]) -> int:
     subdesigns_possible = [0] * (len(design)+1)
     subdesigns_possible[0] = 1
 
     for i in range(1, len(subdesigns_possible)):
         for j in range(i):
             if subdesigns_possible[i-j-1] > 0 and design[i-j-1:i] in patterns:
-                subdesigns_possible[i] = 1
-                break
+                subdesigns_possible[i] +=subdesigns_possible[i-j-1]
 
-    return subdesigns_possible[-1] > 0
+    return subdesigns_possible[-1]
 
 
 if __name__ == '__main__':
@@ -24,8 +23,8 @@ if __name__ == '__main__':
 
     count = 0
     for design in designs:
-        if is_possible(design, patterns):
-            print(design)
-            count += 1
+        arrangement_ways = is_possible(design, patterns)
+        print(design, arrangement_ways)
+        count += arrangement_ways
 
     print(count)
