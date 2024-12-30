ea67c9023100ca1187ab04f864e883316dbb7b9a
pozdn <pozdnyakovpavel@gmail.com>
2024-12-30 14:48:41 +0100

    use list unpacking in solution 2015, day 2

--- 2015/day-02/main.py
+++ 2015/day-02/main.py
@@ -1,17 +1,11 @@
-from functools import reduce
-
-
 def run(file_name: str) -> None:
     with open(file_name) as f:
         paper = 0
         ribbon = 0
         for line in f:
-            nums = sorted([int(n) for n in line.strip().split('x')])
-            a = nums[0] * nums[1]
-            b = nums[0] * nums[2]
-            c = nums[1] * nums[2]
-            paper += 2 * a + 2 * b + 2 * c + a
-            ribbon += reduce(lambda x, y: x * y, nums) + 2 * nums[0] + 2 * nums[1]
+            a, b, c = sorted([int(n) for n in line.strip().split('x')])
+            paper += 2 * a * b + 2 * b * c + 2 * c * a + a * b
+            ribbon += a * b * c + 2 * a + 2 * b
     print(f"paper: {paper}\nribbon: {ribbon}")
 
 
